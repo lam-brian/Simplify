1289d007e886370ba984aa82399e7eec5c2d23e4
nhan <nhanng19@gmail.com>
2022-10-15 22:14:29 -0700

    frontend

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import { Routes, Route, Navigate } from "react-router-dom";
+
+function App() {
+  return (
+    <div>Hello World</div>
+  );
+}
+
+export default App;
