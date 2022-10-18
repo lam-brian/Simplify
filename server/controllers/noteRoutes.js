36875565f12ac4a3e7f47b7770daaf0ae23528fe
lam-brian <brianlam408@gmail.com>
2022-10-17 20:35:19 -0700

    testing

--- server/controllers/noteRoutes.js
+++ server/controllers/noteRoutes.js
@@ -1,15 +1,19 @@
-const router = require('express').Router();
-const { Note } = require('../models')
+const router = require("express").Router();
+const { Note } = require("../models");
+const summarize = require("../utils/summarize");
 
 router.get("/", async (req, res) => {
-    const listOfNotes = await Note.findAll();
-    res.send(listOfNotes)
-})
+  const listOfNotes = await Note.findAll();
+  res.send(listOfNotes);
+});
 
 router.post("/", async (req, res) => {
-    const note = req.body;
-    await Note.create(note);
-    res.json(note);
+  //   const note = req.body;
+  //   await Note.create(note);
+  //   res.json(note);
+  const text = req.body.text;
+  const sum = await summarize(text);
+  res.json({ summary: sum });
 });
 
 module.exports = router;
