33ff947e42fcb0023a7cc4bedd32dfb039a25af7
lam-brian <brianlam408@gmail.com>
2022-10-17 21:00:23 -0700

    setup summarize function

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,13 +1,12 @@
-const router = require('express').Router();
-const userRoutes = require('./userRoutes');
-const noteRoutes = require('./noteRoutes');
+const router = require("express").Router();
+const userRoutes = require("./userRoutes");
+const noteRoutes = require("./noteRoutes");
 
-router.use('/users', userRoutes);
-router.use('./notes', noteRoutes);
-
+router.use("/users", userRoutes);
+router.use("/notes", noteRoutes);
 
 router.get("/", (req, res) => {
-    res.json("Hello World");
-})
+  res.json("Hello World");
+});
 
 module.exports = router;
--- server/controllers/noteRoutes.js
+++ server/controllers/noteRoutes.js
@@ -1,15 +1,20 @@
-const router = require('express').Router();
-const { Note } = require('../models')
+const router = require("express").Router();
+const { Note } = require("../models");
+const summarize = require("../utils/summarize");
 
 router.get("/", async (req, res) => {
-    const listOfNotes = await Note.findAll();
-    res.send(listOfNotes)
-})
+  const listOfNotes = await Note.findAll();
+  res.send(listOfNotes);
+});
 
 router.post("/", async (req, res) => {
-    const note = req.body;
-    await Note.create(note);
-    res.json(note);
+  // const note = req.body;
+  // await Note.create(note);
+  // res.json(note);
+
+  const { text } = body.req;
+  const summary = await summarize(text);
+  res.json({ summary });
 });
 
 module.exports = router;
