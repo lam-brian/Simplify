6f453332e6ae15a301f01c55c176d68275f99dd8
nhan <nhanng19@gmail.com>
2022-10-21 21:57:40 -0700

    added homeroutes

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
-
 import Summarize from "./pages/Summarize";
 import Layout from "./components/Layout/Layout";
+import HomePage from "./pages/HomePage"
 
 function App() {
   return (
@@ -10,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Navigate to="/summarize" />} />
         <Route path="/summarize" element={<Summarize />} />
+        <Route path="/home" element={<HomePage />} />
       </Routes>
     </Layout>
   );
