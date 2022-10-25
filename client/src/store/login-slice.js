566559c67a5fbdb147a3d67872aa801b250acced
lam-brian <brianlam408@gmail.com>
2022-10-25 04:36:56 -0700

    initialize store

---
+++ client/src/store/login-slice.js
@@ -0,0 +1,22 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  isLoggedIn: false,
+};
+
+const loginSlice = createSlice({
+  name: "login",
+  initialState,
+  reducers: {
+    login(state) {
+      state.isLoggedIn = true;
+    },
+    logout(state) {
+      state.isLoggedIn = false;
+    },
+  },
+});
+
+export const loginActions = loginSlice.actions;
+
+export default loginSlice;
