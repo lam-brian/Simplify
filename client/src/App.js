c85ad957164953aea7ae737724e4e39b11c8f022
lam-brian <brianlam408@gmail.com>
2022-10-25 11:01:44 -0700

    update routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,36 @@
 import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+
 import Summarize from "./pages/Summarize";
 import Layout from "./components/Layout/Layout";
 import HomePage from "./pages/HomePage";
 
 function App() {
-  return (
-    <Layout>
-      <Routes>
-        <Route path="/" element={<Navigate to="/summarize" />} />
+  const isLoggedIn = useSelector((state) => state.login.isLoggedIn);
+
+  let routes;
+
+  if (isLoggedIn) {
+    routes = (
+      <>
+        <Route path="/" element={<Navigate to="/home" />} />
         <Route path="/summarize" element={<Summarize />} />
         <Route path="/home" element={<HomePage />} />
-      </Routes>
+      </>
+    );
+  } else {
+    routes = (
+      <>
+        <Route path="/" element={<Navigate to="/summarize" />} />
+        <Route path="/summarize" element={<Summarize />} />
+      </>
+    );
+  }
+
+  return (
+    <Layout>
+      <Routes>{routes}</Routes>
     </Layout>
   );
 }
