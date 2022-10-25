56a0057e33789e5c6b1dd4ace09294c708a9d1d1
nhan <nhanng19@gmail.com>
2022-10-25 16:35:29 -0700

    welcome screen

--- client/src/components/Notes/Home.js
+++ client/src/components/Notes/Home.js
@@ -1,9 +1,29 @@
 import styles from "./Home.module.css"
+import Typing from "../../images/typing.png"
+import Notes from "./Notes"
+
 
 const Home = () => {
     return (
       <>
-        Home
+        <div className={styles.container}>
+          <div className={styles.card}>
+            <div className={styles.frame}>
+              <div className={styles.welcome}>Welcome back, Brian</div>
+              <div className={styles.start}>Let's start studying</div>
+            </div>
+            <img className={styles.typing} src={Typing} />
+          </div>
+        </div>
+        <div className={styles.header}>Your notes</div>
+        <div className={styles.cardFrame}>
+          <Notes />
+          <Notes />
+          <Notes />
+          <Notes />
+          <Notes />
+          <Notes />
+        </div>
       </>
     );
 }
