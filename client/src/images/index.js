b137e387f5ff6e436544754ee005d9cca485f360
lam-brian <brianlam408@gmail.com>
2022-10-25 05:57:43 -0700

    add icons

--- client/src/images/index.js
+++ client/src/images/index.js
@@ -1,6 +1,7 @@
 import logo from "./logo.svg";
+import logoLogged from "./logo-logged.svg";
 import dictionary from "./illustration-dictionary.svg";
 import books from "./illustration-books.svg";
 
-export const simplifyLogo = logo;
+export const logos = { logo, logoLogged };
 export const illustrations = { dictionary, books };
