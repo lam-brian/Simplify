d03a6c05d7e2f6bdc9167c9ce07a567698febc55
lam-brian <brianlam408@gmail.com>
2022-10-25 07:56:04 -0700

    export icons in index.js

--- client/src/images/index.js
+++ client/src/images/index.js
@@ -1,7 +1,40 @@
+// logos
 import logo from "./logo.svg";
 import logoLogged from "./logo-logged.svg";
+
+//illustrations
 import dictionary from "./illustration-dictionary.svg";
 import books from "./illustration-books.svg";
 
+// icons
+import arrowDown from "./icons/arrow-down.svg";
+import arrowUp from "./icons/arrow-up.svg";
+import closeCircle from "./icons/close-circle.svg";
+import documentText from "./icons/document-text.svg";
+import edit from "./icons/edit.svg";
+import eyeSlash from "./icons/eye-slash.svg";
+import eye from "./icons/eye.svg";
+import home from "./icons/home.svg";
+import logout from "./icons/logout.svg";
+import notification from "./icons/notification.svg";
+import search from "./icons/search.svg";
+import star from "./icons/star.svg";
+import user from "./icons/user.svg";
+
 export const logos = { logo, logoLogged };
 export const illustrations = { dictionary, books };
+export const icons = {
+  arrowDown,
+  arrowUp,
+  closeCircle,
+  documentText,
+  edit,
+  eyeSlash,
+  eye,
+  home,
+  logout,
+  notification,
+  search,
+  star,
+  user,
+};
