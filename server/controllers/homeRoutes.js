81998bd487b905a827ad31d7b295b73cc8d63133
nhan <nhanng19@gmail.com>
2022-10-26 23:07:58 -0700

    added keyword table

--- server/controllers/homeRoutes.js
+++ server/controllers/homeRoutes.js
@@ -1,18 +1,30 @@
 const router = require("express").Router();
-const { Note, User } = require("../models");
+const { Note, User, Keyword } = require("../models");
 
 router.get("/notes", async (req, res) => {
   try {
-    const userData = await User.findByPk(req.session.user_id, {
-      attributes: { exclude: ["password"] },
-      include: [{ model: Note }],
-    });
-    const user = userData.get({ plain: true });
-    console.log(user);
-    res.status(200).json(user);
+    const notes = await Note.findAll({ include: User });
+    res.json(notes);
   } catch (err) {
-    res.status(500).json(err);
+    console.error(err);
+    res.status(500).json({
+      error: true,
+      message: "Couldn't get notes.",
+    });
   }
+});
+
+router.post("/keyword", async (req, res) => {
+  const keyword = req.body;
+  await Keyword.create(keyword);
+  res.json(keyword);
+});
+
+
+router.get("/keyword/:noteId", async (req, res) => {
+  const noteId = req.params.noteId;
+  const keywords = await Keyword.findAll({ where: { note_id: noteId } });
+  res.json(keywords);
 });
 
 module.exports = router;
