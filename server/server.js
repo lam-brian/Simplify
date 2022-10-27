91d87e1b7091ccc693a27535eb32395472d3581e
lam-brian <brianlam408@gmail.com>
2022-10-27 08:52:01 -0700

    update origin

--- server/server.js
+++ server/server.js
@@ -26,7 +26,7 @@ const sess = {
 };
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: `${process.env.CLIENT_URL}`,
     methods: ["POST", "PUT", "GET", "OPTIONS", "HEAD"],
     credentials: true,
   })
